b4034e3b6e712f7bcba204b619286b36f7ddaf23
Kartik Madhira <kartikmadhira1@gmail.com>
2019-09-11 23:20:52 -0400

    added opencv to cmakelists

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,21 @@
 // Copyright 2019
 
-#include <iostream>
-#include "core.h" //NOLINT
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
 
-int main(int argc, char** argv) {
-  int val = add(1, 3);
-
-  Vector2 vec(0.0, 0.0);
-  vec.set(2.5, 1.0);
-  std::tuple<float, float> coords = vec.get();
-
-  std::cout << val << std::endl;
-  std::cout << std::get<0>(coords) << std::endl;
-  std::cout << std::get<1>(coords) << std::endl;
-
-  return 0;
+int main(int argc, char** argv ) {
+    if ( argc != 2 ) {
+        printf("usage: DisplayImage.out <Image_Path>\n");
+        return -1;
+    }
+    cv::Mat image;
+    image = cv::imread(argv[1], 1);
+    if (!image.data) {
+        printf("No image data \n");
+        return -1;
+        }
+    cv::namedWindow("new image", cv::WINDOW_AUTOSIZE);
+    cv::imshow("Display Image", image);
+    cv::waitKey(0);
+    return 0;
 }
